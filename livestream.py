af777b71a6713e1b32b49ca3bd39fef1f98e202f
SkippySteve <69158610+SkippySteve@users.noreply.github.com>
2022-03-26 21:13:11 +0000

    Add files via upload

---
+++ livestream.py
@@ -0,0 +1,30 @@
+#import sys module for args
+import sys
+#import os module for getting list of files in dir
+import os
+#import subprocess for issuing ffmpeg commands
+import subprocess
+#import random for shuffling
+import random
+
+if len(sys.argv) < 4:
+	print("Less than needed number of arguments provided. Example: python livestream.py mp3.Directory.Here ImageHere StreamKeyHere")
+	sys.exit()
+
+
+dir_list = os.listdir(sys.argv[1])
+mp3List = []
+
+print("Input dir for mp3s: ", sys.argv[1])
+print("Image to use: ", sys.argv[2])
+
+for file in dir_list:
+	if file[-4:] == ".mp3":
+		mp3List.append(file)
+
+print("Found ", len(mp3List), " mp3s in provided dir")
+
+numNotOne = 0
+
+while numNotOne != 1: #just ensuring the loop never ends...
+	subprocess.run(["ffmpeg", "-loglevel", "error", "-y", "-re", "-loop", "1", "-f", "image2", "-i", sys.argv[2], "-i", str(sys.argv[1]+mp3List[random.randrange(len(mp3List))]), "-codec:a", "copy", "-shortest", "-f", "flv", str("rtmp://a.rtmp.youtube.com/live2/"+sys.argv[3])])
