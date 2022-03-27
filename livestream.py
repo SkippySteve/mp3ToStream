7af9c0a68326add287fd0ce782a5747062adb6cd
SkippySteve <69158610+SkippySteve@users.noreply.github.com>
2022-03-27 00:39:37 +0000

    Update livestream.py

--- livestream.py
+++ livestream.py
@@ -23,4 +23,6 @@ print("Found ", len(mp3List), " mp3s in provided dir")
 numNotOne = 0
 
 while numNotOne != 1:	#just ensuring the loop never ends...
-	subprocess.run(["ffmpeg", "-loglevel", "error", "-y", "-re", "-loop", "1", "-f", "image2", "-i", sys.argv[2], "-i", str(sys.argv[1]+mp3List[random.randrange(len(mp3List))]), "-codec:a", "copy", "-shortest", "-f", "flv", str("rtmp://a.rtmp.youtube.com/live2/"+sys.argv[3])])
+	currentTrack = mp3List[random.randrange(len(mp3List))]
+	print("Currently playing: ", currentTrack)
+	subprocess.run(["ffmpeg", "-loglevel", "error", "-y", "-re", "-loop", "1", "-f", "image2", "-i", sys.argv[2], "-i", str(sys.argv[1]+currentTrack), "-codec:a", "copy", "-shortest", "-f", "flv", str("rtmp://a.rtmp.youtube.com/live2/"+sys.argv[3])])
